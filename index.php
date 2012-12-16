d31ff4f87cf75441adfebc064a0d0bc605af172d
Jonathan Dallas <github@jonathandallas.com>
2012-12-16 11:07:38 -0500

    Copy updates to About Us

--- index.php
+++ index.php
@@ -13,21 +13,19 @@
 	<div id="mainText">
 		<div id="subtitle">About Us</div>
 		
-    <div id="content"> We are a support group for people with chronic pain in the 
-      Boston, MA area. Once a month, we meet to discuss the challenges of living 
-      with chronic pain and offer support to each other. At our meetings, we can 
-      also hear updates on research and therapies from physicians and other professionals 
-      in the area. 
+    <div id="content"> We are a support group for people living with chronic pain in the Boston, MA area and surrounding New England area. 
+Our group was founded in April, 2000 and has been meeting continuously year round since then.
+Once a month, we meet to discuss the challenges of living with chronic pain and offer support to each other. 
+At our meetings, we can also hear updates on research and therapies from physicians and other professionals in the area. 
       <p>Our mission is to provide a safe place where people with chronic pain 
         can support one another, develop friendships, share ideas and thoughts, 
         obtain information about their condition from professionals and each other, 
         and learn coping skills in a nonjudgmental and caring atmosphere. </p>
       <p>Our meetings are free and open to anyone living with chronic pain. We 
         meet at Robbins Library in Arlington, MA on the second Friday of the month, 
-        from 11:00am to 12:30pm. When that library is busy, we meet at the Fox 
-        Library in a downstairs room or the Jefferson Cutter House in Arlington Center. <a href="directions.php">Click here</a> 
-        for directions</p>
-      <p>We are a member of the American Chronic Pain Association, founded in 
+        from 11:00am to 12:30pm. When that location is unavailable,
+we meet at an alternate location in Arlington.</p>
+      <p>We are a chapter of the American Chronic Pain Association, founded in 
         1980,which is an international, non-profit, self-help organization that 
         offers educational materials and peer support groups. The ACPA is dedicated 
         to raising awareness about issues of living with chronic pain among the 
