35e821fda8fbccd800de0e4d1b0cc50f52931111
Jonathan Dallas <github@jonathandallas.com>
2012-12-16 11:08:57 -0500

    Archiving 2012 meeting schedule

---
+++ meetings-2012.php
@@ -0,0 +1,44 @@
+<?php echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>American Chronic Pain Association - Boston Chapter</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+
+<link rel="shortcut icon" href="favicon.ico" >
+<link href="include/acpa.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div id="background">
+	<div id="mainText">
+		
+    <div id="subtitle">Schedule of Meetings</div>
+		
+    <div id="content">We meet the second Friday of every month from 11:00 am to 
+      12:30 pm at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins 
+      (main) Library</a>, 700 Mass Ave. in the ground floor Community Room. Please 
+      note that meetings marked with an asterisk will be held at an alternate location. 
+      Please mark your calendar!<br />
+      <h3>2012 Meeting Schedule:</h3>
+      <p>January 13th - PainACTION Website Creators will speak about topics covered on the site; we will discuss how selected topics can be helpful to people with pain. </p>
+      <p>February 10th</p>
+      <p>March 9th</p>
+      <p>April 13th</p>
+      <p>May 11th</p>
+	  <p>June 8th</p>
+      <p>July 13th</p>
+	  <p>August 10th</p>
+      <p>September 14th *</p>
+	  <p>October 12th</p>
+	  <p>November 9th</p>
+      <p>December 14th</p>
+      <p class="finePrint">* Meeting at Jefferson Cutter House, Arlington Center, corner of Mystic Street and Mass Ave, rear entrance</p>
+      <p class="finePrint">** Meeting will be held at Sunrise Assisted Living in Arlington Heights at 1395 Mass Ave, Arlington, MA in the Activity Room; there is a large free parking lot on the premises.</p>
+    </div>
+	</div>
+</div>
+<?php include ('include/acpa.php'); ?>
+
+</body>
+</html>
