6a463f7caa78fa5e73b062ee0fec4a00117b0503
Jonathan Dallas <web@jonathandallas.com>
2014-12-08 20:39:56 -0500

    Archive of 2014 meetings

---
+++ meetings-2014.php
@@ -0,0 +1,45 @@
+<?php echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>American Chronic Pain Association - Boston Chapter</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+
+<link rel="shortcut icon" href="favicon.ico" >
+<link href="include/acpa.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div id="background">
+	<div id="mainText">
+		
+    <div id="subtitle">Schedule of Meetings</div>
+		
+    <div id="content">We meet the second Friday of every month from 11:00 am to 
+      12:30 pm at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins 
+      (main) Library</a>, 700 Mass Ave. in the ground floor Community Room. Please 
+      note that meetings marked with an asterisk will be held at an alternate location. 
+      Please mark your calendar!<br />
+      <h3>2014 Meeting Schedule:</h3>
+      <p>January 10th</p>
+      <p>February 14th - cancelled due to weather</p>
+      <p>March 14th - Judy Foreman, Syndicated Health Columnist will be speaking about her forthcoming book "A Nation In Pain: Healing Our Biggest Health Problem" and signing copies of her book.</p>
+      <p>April 11th</p>
+      <p>May 9th</p>
+	  <p>June 13th</p>
+      <p>July 11th</p>
+	  <p>August 8th</p>
+      <p>September 12th - Guest Speaker - Pharmacist Larry Maida on Therapy Options for Chronic Pain.*</p>
+	  <p>October 10th</p>
+	  <p>November 14th</p>
+      <p>December 12th</p>
+      <p class="finePrint">* this meeting will be held at the
+Sunrise Assisted Living in Arlington at 1395 Massachusetts Ave.  Free
+parking next to the building.</p>
+    </div>
+	</div>
+</div>
+<?php include ('include/acpa.php'); ?>
+
+</body>
+</html>
