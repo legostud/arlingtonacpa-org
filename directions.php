e55d67cbb2493efc10ed96129ec541c138705c9e
Jonathan Dallas <jonathan.dallas@velir.com>
2017-02-28 15:49:30 -0500

    Upgrade map to newer version

--- directions.php
+++ directions.php
@@ -5,102 +5,10 @@
 <title>American Chronic Pain Association - Boston Chapter</title>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 
-<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAkgILOEVwkjfFsx7QeRUnchTQpbje7hmvjDlXSnqlNWxYRcLx_xR_D5R10WRSSUhMsB3lyn2SYy24eQ" type="text/javascript"></script>
-<script type="text/javascript">
-
-    //<![CDATA[
-// http://stevemorse.org/jcal/latlon.php - tool to get Lat and Long codes
-var centerMap = new GLatLng(42.417196,-71.168398);
-
-//Robins Library Variables
-var robbinsLibrary = new GLatLng(42.416169, -71.155983);
-// Create our custom marker icon
-var robbinsIcon = new GIcon(G_DEFAULT_ICON);
-robbinsIcon.image = "image/markerBlueR.png";
-markerOptions = { icon:robbinsIcon };
-var robbinsMarker = new GMarker(robbinsLibrary,markerOptions);
-var robbinsMessage = "<a href=\"http://www.robbinslibrary.org\" target=\"_blank\">Robbins Library</a><br />700 Mass Ave<br />Arlington, MA 02476";
-
-//Fox Library Variables
-var foxLibrary = new GLatLng(42.405519,-71.141689);
-// Create our custom marker icon
-var foxIcon = new GIcon(G_DEFAULT_ICON);
-foxIcon.image = "image/markerBlueF.png";
-markerOptions = { icon:foxIcon };
-var foxMarker = new GMarker(foxLibrary,markerOptions);
-var foxMessage = "<a href=\"http://www.robbinslibrary.org/about/fox-branch\" target=\"_blank\">Fox Library</a><br />175 Mass Ave<br />East Arlington, MA 02474";
-
-//Jefferson House Variables
-var jeffersonHouse = new GLatLng(42.4154392,-71.1525275);
-// Create our custom marker icon
-var jeffersonIcon = new GIcon(G_DEFAULT_ICON);
-jeffersonIcon.image = "image/markerBlueJ.png";
-markerOptions = { icon:jeffersonIcon };
-var jeffersonMarker = new GMarker(jeffersonHouse,markerOptions);
-var jeffersonMessage = "<a href=\"http://www.town.arlington.ma.us/public_documents/ArlingtonMA_Rental/JeffersonCutterHouse/index\" target=\"_blank\">Jefferson Cutter House</a><br />Corner of Mystic St. and Mass Ave<br />Arlington, MA 02476";
-
-//Sunrise Assisted Living
-var salHouse = new GLatLng(42.424595,-71.184024);
-// Create our custom marker icon
-var salIcon = new GIcon(G_DEFAULT_ICON);
-salIcon.image = "image/markerBlueS.png";
-markerOptions = { icon:salIcon };
-var salMarker = new GMarker(salHouse,markerOptions);
-var salMessage = "<a href=\"http://www.sunriseseniorliving.com\" target=\"_blank\">Sunrise Assisted Living</a><br />1395 Mass Ave<br />Arlington, MA 02476";
-
-// Arlington Senior Center
-var asCenter = new GLatLng(42.415132, -71.156809);	
-// Create our custom marker icon
-var acIcon = new GIcon(G_DEFAULT_ICON);
-acIcon.image = "image/markerBlueA.png";
-markerOptions = { icon:acIcon };
-var acMarker = new GMarker(asCenter,markerOptions);
-var acMessage = "<a href=\"http://arlingtonseniorsassociation.org\" target=\"_blank\">Arlington Senior Center</a><br />27 Maple Street<br />Arlington, MA 02476";
-
-function load() {
-	if (GBrowserIsCompatible()) {
-		var map = new GMap2(document.getElementById("map"));
-    	//zoom controls
-		map.addControl(new GLargeMapControl());
-    	//center the map halfway between the two libraries	
-		map.setCenter(centerMap, 14);
-		
-		createMarkers(map);
-	}
-}
-
-function createMarkers(map) {
-	GEvent.addListener(robbinsMarker, "click", function() {
-    	map.openInfoWindowHtml(robbinsLibrary, robbinsMessage);
-  	});
-	//GEvent.addListener(foxMarker, "click", function() {
-    //	map.openInfoWindowHtml(foxLibrary, foxMessage);
-  	//});
-	GEvent.addListener(jeffersonMarker, "click", function() {
-    	map.openInfoWindowHtml(jeffersonHouse, jeffersonMessage);
-  	});
-	GEvent.addListener(salMarker, "click", function() {
-    	map.openInfoWindowHtml(salHouse, salMessage);
-  	});
-  GEvent.addListener(acMarker, "click", function() {
-      map.openInfoWindowHtml(asCenter, acMessage);
-    });
-	map.addOverlay(robbinsMarker);
-	//map.addOverlay(foxMarker);
-	//map.addOverlay(jeffersonMarker);
-	map.addOverlay(salMarker);
-  map.addOverlay(acMarker);	
-  //show the Robbins libary Info Window by default
-   //map.openInfoWindowHtml(robbinsLibrary, robbinsMessage);
-}
-
-    //]]>
-</script>
-
 <link rel="shortcut icon" href="favicon.ico" >
 <link href="include/acpa.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-#map {
+.google-map {
 	width: 725px; 
 	height: 650px;
 	clear: both;
@@ -110,14 +18,18 @@ function createMarkers(map) {
 	width:32%;
 	height:75px;
 }
+
+.info-window h3 {
+  margin-top: 0;
+}
 </style>
 
 </head>
-<body onload="load()" onunload="GUnload()">
+<body>
 <div id="background">
 	<div id="mainText">
 		
-<div id="subtitle">Meeting Locations</div>
+  <div id="subtitle">Meeting Locations</div>
 		
     <div id="content"> Our Meetings are usually held at the Robbins Library in 
       Arlington, Ma. with the occasional meeting at the Arlington Senior Center or Sunrise Assisted Living.<br />
@@ -130,22 +42,114 @@ function createMarkers(map) {
       	Fox Library</a><br />
         175 Mass Ave<br />
         Arlington, MA 02474</div -->
-      <div class="address"><a href="http://arlingtonseniorsassociation.org" target="_blank">
+      <!-- div class="address"><a href="http://arlingtonseniorsassociation.org" target="_blank">
       	Arlington Senior Center</a><br />
         27 Maple Street<br />
-		Arlington, MA 02476</div>
+		Arlington, MA 02476</div -->
       <div class="address">
         <a href="http://www.sunriseseniorliving.com" target="_blank">Sunrise Assisted Living</a><br />
-      	1395 Mass Ave<br />
-      	Arlington, MA 02476
+        1395 Mass Ave<br />
+        Arlington, MA 02476
+      </div>
+      <div class="address">
+        <a href="http://www.arlingtonma.gov/departments/police\" target=\"_blank\">Arlington Police Building</a><br />112 Mystic Street<br />
+        Arlington, MA 02474
       </div>
 
-<!-- the map layer is dynamically loaded with a google map - leave blank -->
-			<div id="map"></div>
+      <!-- the map layer is dynamically loaded with a google map - leave blank -->
+      <div class="google-map js-google-map"></div>
+      <script>
+        var googleMapData = googleMapData || [];
+
+        googleMapData.push({
+          map: {
+            center: {
+              lat: 42.417196,
+              lng: -71.168398
+            },
+            zoom: 14
+          },
+          markers: [
+            {
+              position: {
+                lat: 42.416169, 
+                lng: -71.155983 
+              },
+              label: "R", // single character only
+              infoWindow: {
+                name: "Robbins Library", // raw was needed to prevent rendering ascii characters
+                link: "http://www.robbinslibrary.org",
+                address: "700 Mass Ave<br />Arlington, MA 02476"
+              }
+            },
+            // {
+            //   position: {
+            //     lat: 42.405519, 
+            //     lng: -71.141689 
+            //   },
+            //   label: "F", // single character only
+            //   infoWindow: {
+            //     name: "Fox Library", // raw was needed to prevent rendering ascii characters
+            //     link: "http://www.robbinslibrary.org/about/fox-branch",
+            //     address: "175 Mass Ave<br />East Arlington, MA 02474"
+            //   }
+            // },
+            // {
+            //   position: {
+            //     lat: 42.4154392, 
+            //     lng: -71.1525275 
+            //   },
+            //   label: "J", // single character only
+            //   infoWindow: {
+            //     name: "Jefferson Cutter House", // raw was needed to prevent rendering ascii characters
+            //     link: "http://www.town.arlington.ma.us/public_documents/ArlingtonMA_Rental/JeffersonCutterHouse/index",
+            //     address: "Corner of Mystic St. and Mass Ave<br />Arlington, MA 02476"
+            //   }
+            // },
+            {
+              position: {
+                lat: 42.424595, 
+                lng: -71.184024 
+              },
+              label: "S", // single character only
+              infoWindow: {
+                name: "Sunrise Assisted Living", // raw was needed to prevent rendering ascii characters
+                link: "http://www.sunriseseniorliving.com",
+                address: "1395 Mass Ave<br />Arlington, MA 02476"
+              }
+            },
+            // {
+            //   position: {
+            //     lat: 42.415132, 
+            //     lng: -71.156809 
+            //   },
+            //   label: "A", // single character only
+            //   infoWindow: {
+            //     name: "Arlington Senior Center",
+            //     link: "http://arlingtonseniorsassociation.org",
+            //     address: "27 Maple Street<br />Arlington, MA 02476"
+            //   }
+            // },
+            {
+              position: {
+                lat: 42.419428, 
+                lng: -71.152037 
+              },
+              label: "P", // single character only
+              infoWindow: {
+                name: "Arlington Police Building",
+                link: "http://www.arlingtonma.gov/departments/police",
+                address: "112 Mystic Street<br />Arlington, MA 02474"
+              }
+            }
+          ]
+        });
+      </script>
+
 		</div>
 	</div>
 </div>
 <?php include ('include/acpa.php'); ?>
-
+<script src="/include/google-map.js"></script>
 </body>
 </html>
---
+++ include/google-map.js
@@ -0,0 +1,66 @@
+(function(window,document,undefined) {
+
+  // only run this code if there is a google map component on the page
+  var gMapEl = document.querySelector('.js-google-map');
+  
+  if(typeof gMapEl === 'undefined'){
+    return;
+  }
+
+  // after the api is loaded this function is called
+  window.initMap = function() {
+
+    // get the maps data
+    // this could be replaced with an api
+    var rawData = googleMapData[0];
+      
+    // *** Create the Map *** //
+    // map defaults
+    var initMapData = {
+      scrollwheel: false
+    }
+
+    // create map Data by combining the rawData with the defaults
+    var mapData = Object.assign({}, rawData.map, initMapData);
+
+    var map = new google.maps.Map(gMapEl, mapData);
+
+    var markers = [];
+
+    // *** Add Markers with popups *** //
+    rawData.markers.forEach(function(d,i){
+      var markerData = Object.assign({map},d);
+
+      var marker =  new google.maps.Marker(markerData);
+
+      var template = [
+        '<section class="info-window">',
+          '<h3 class="info-window__name">'+ markerData.infoWindow.name +'</h3>',
+          '<div class="ma__info-window__address">' + markerData.infoWindow.address + '</div>',
+        '</section>'
+      ].join(" ");
+
+      var infoWindow = new google.maps.InfoWindow({
+        content: template
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
+
+      marker.showInfo = () => {
+        infoWindow.open(map, marker);
+      }
+
+      markers.push(marker);
+    });
+  }
+
+  // load Google's api
+  var script = document.createElement('script');
+    script.src = "//maps.googleapis.com/maps/api/js?key=AIzaSyDNDQepPfh0XsEXA0_S2UOFR852dlr1WUg&callback=initMap";
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+
+}(window,document));
+
