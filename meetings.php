d9748674c437f7c7cbfdd6c5ca9aea16b041781b
Jonathan Dallas <jonathan.dallas@velir.com>
2017-09-05 19:45:59 -0400

    Updated September meeting schedule

--- meetings.php
+++ meetings.php
@@ -18,19 +18,19 @@
         <h3>2017 Meeting Schedule:</h3>
         <p><b>January 13th</b> &mdash; Guests: Chris Ellinger &amp; True Story Theater, What Sustains Us When We Have Chronic Pain?</p>
         <p><b>February 10th</b> &mdash; CANCELLED due to weather</p>
-        <p><b>March 10th*</b> &mdash; Guest Speaker: Sonya Huber, Author of Pain Woman Takes Your Keys &amp; Other Essays from a Nervous System <br /> - <i>This meeting will be held at the Arlington Police Building, 112 Mystic Street, Arlingtn in the Community Conference Room on the 2nd floor</i></p>
-        <p><b>April 14th</b> &mdash; 17 Year Anniversary Cake &amp; Party &amp; Open Discussion<br />
-        ++++Time change: Meeting begins at 10am - 11:30am Library closing at noon++++</p>
+        <p><b>March 10th</b> &mdash; Guest Speaker: Sonya Huber, Author of Pain Woman Takes Your Keys &amp; Other Essays from a Nervous System <br /> - <i>This meeting will be held at the Arlington Police Building, 112 Mystic Street, Arlington in the Community Conference Room on the 2nd floor</i></p>
+        <p><b>April 14th</b> &mdash; 17 Year Anniversary Cake &amp; Party &amp; Open Discussion</p>
         <p><b>May 12th</b> &mdash; Dr. David Nagel, Author of Needless Suffering: How Society Fails Those With Chronic Pain</p>
         <p><b>June 9th</b> &mdash; Open Discussion</p>
         <p><b>July 14th</b> &mdash; Summer Open Discussion</p>
         <p><b>August 11th</b> &mdash; Summer Open Discussion</p>
-        <p><b>September 8th</b> &mdash; Guest Speaker: Liz Owen, Yoga for Chronic Pain</p>
+        <p><b>September 8th*</b> &mdash; Guest Speaker: Liz Owen, Yoga for Chronic Pain<br />
+        ++++Location change: Meeting will be held at St Paul Lutheran Church at 929 Concord Turnpike in Arlington (see directions below)++++</p>
         <p><b>October 13th</b> &mdash; Guest Speaker: Gianna D’Errico, D.C., Chiropractic for Chronic Pain</p>
         <p><b>November 10th</b> &mdash; Guest Speaker: Chris Veasley, “Multiple Chronic Pain Conditions Research and Advocacy”</p>
         <p><b>December 8th</b> &mdash; Holiday Party &amp; Discussion of Managing the Holidays with Pain</p>
         <!-- p class="finePrint">* This meeting will be held at the Sunrise Assisted Living Center at 1395 Massachusetts Ave, Arlington, MA 02476</p -->
-        <small class="finePrint">* This meeting will be held at the Arlington Police Building, 112 Mystic Street, Arlington in the Community Conference Room on the 2nd floor; building is handicaped accessible</small>
+        <small class="finePrint">* This meeting will be held at the St Paul Lutheran Church, 929 Concord Turnpike, Arlington; building is handicaped accessible &amp; there is on-site parking.</small>
       </div>
     </section>
   </main>
