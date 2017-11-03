7e65a97a2b581a79f5d8c017776c1f9c46db66fd
Jonathan Dallas <jonathan.dallas@velir.com>
2017-11-03 13:49:07 -0400

    Update for November

--- meetings.php
+++ meetings.php
@@ -27,7 +27,7 @@
         <p><b>September 8th*</b> &mdash; Guest Speaker: Liz Owen, Yoga for Chronic Pain<br />
         ++++Location change: Meeting will be held at St Paul Lutheran Church at 929 Concord Turnpike in Arlington (see directions below)++++</p>
         <p><b>October 13th</b> &mdash; Guest Speaker: Gianna D’Errico, D.C., Chiropractic for Chronic Pain</p>
-        <p><b>November 10th</b> &mdash; Guest Speaker: Chris Veasley, “Multiple Chronic Pain Conditions Research and Advocacy”</p>
+        <p><b>November 10th</b> &mdash; Open Discussion</p>
         <p><b>December 8th</b> &mdash; Holiday Party &amp; Discussion of Managing the Holidays with Pain</p>
         <!-- p class="finePrint">* This meeting will be held at the Sunrise Assisted Living Center at 1395 Massachusetts Ave, Arlington, MA 02476</p -->
         <small class="finePrint">* This meeting will be held at the St Paul Lutheran Church, 929 Concord Turnpike, Arlington; building is handicaped accessible &amp; there is on-site parking.</small>
