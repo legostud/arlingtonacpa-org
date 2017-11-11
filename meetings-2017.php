e9943893bd5c1ad3e3a6e52d9c12b9df0634a633
Jonathan Dallas <jonathan.dallas@velir.com>
2017-11-11 15:44:23 -0500

    Archiving 2017 Meeting schedule

---
+++ meetings-2017.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Schedule of Meetings - Chronic Pain Support Group, Arlington, MA</title>
+  <?php include './include/head.php' ?>
+</head>
+<body>
+  <?php include './include/header.php' ?>
+  <main class="main">
+    <div class="main__nav">
+      <?php include './include/main-nav.php' ?>
+    </div>
+    <section class="main__content" id="main-content">
+      <div class="main__title"><h1>Schedule of Meetings</h1></div>
+      <div class="main__text rich-text">
+        <p>We meet the second Friday of every month from 11:00am to 12:30pm with an optional 12:30pm to 1:30pm social hour at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins (main) Library</a>, 700 Mass Ave. in the ground floor Community Room.  Please note that meetings marked with an asterisk will be held at an alternate location.  Please mark your calendar!</p>
+        <p>&nbsp;</p>
+        <h3>2017 Meeting Schedule:</h3>
+        <p><b>January 13th</b> &mdash; Guests: Chris Ellinger &amp; True Story Theater, What Sustains Us When We Have Chronic Pain?</p>
+        <p><b>February 10th</b> &mdash; CANCELLED due to weather</p>
+        <p><b>March 10th</b> &mdash; Guest Speaker: Sonya Huber, Author of Pain Woman Takes Your Keys &amp; Other Essays from a Nervous System <br /> - <i>This meeting will be held at the Arlington Police Building, 112 Mystic Street, Arlington in the Community Conference Room on the 2nd floor</i></p>
+        <p><b>April 14th</b> &mdash; 17 Year Anniversary Cake &amp; Party &amp; Open Discussion</p>
+        <p><b>May 12th</b> &mdash; Dr. David Nagel, Author of Needless Suffering: How Society Fails Those With Chronic Pain</p>
+        <p><b>June 9th</b> &mdash; Open Discussion</p>
+        <p><b>July 14th</b> &mdash; Summer Open Discussion</p>
+        <p><b>August 11th</b> &mdash; Summer Open Discussion</p>
+        <p><b>September 8th*</b> &mdash; Guest Speaker: Liz Owen, Yoga for Chronic Pain<br />
+        ++++Location change: Meeting will be held at St Paul Lutheran Church at 929 Concord Turnpike in Arlington (see directions below)++++</p>
+        <p><b>October 13th</b> &mdash; Guest Speaker: Gianna D’Errico, D.C., Chiropractic for Chronic Pain</p>
+        <p><b>November 10th</b> &mdash; Open Discussion</p>
+        <p><b>December 8th</b> &mdash; Holiday Party &amp; Discussion of Managing the Holidays with Pain</p>
+        <!-- p class="finePrint">* This meeting will be held at the Sunrise Assisted Living Center at 1395 Massachusetts Ave, Arlington, MA 02476</p -->
+        <small class="finePrint">* This meeting will be held at the St Paul Lutheran Church, 929 Concord Turnpike, Arlington; building is handicaped accessible &amp; there is on-site parking.</small>
+      </div>
+    </section>
+  </main>
+  <?php include './include/foot.php' ?>
+</body>
+</html>
