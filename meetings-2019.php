4d747a25aa0441261351795e22f676bca060d362
Jonathan Dallas <jonathan.dallas@velir.com>
2020-03-16 09:07:38 -0400

    2020 meeting schedule

--- meetings-2019.php
+++ meetings-2019.php
@@ -15,22 +15,23 @@
       <div class="main__text rich-text">
         <p>We meet the second Friday of every month from 11:00am to 12:30pm with an optional 12:30pm to 1:30pm social hour at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins (main) Library</a>, 700 Mass Ave. in the ground floor Community Room.  Please note that meetings marked with an asterisk will be held at an alternate location.  Please mark your calendar!</p>
         <p>&nbsp;</p>
-        <h3>2020 Meeting Schedule:</h3>
-        <p><b>January 10</b></p>
-        <p><b>February 14</b></p>
-        <p><b>March 13</b></p>
-        <p><b>April 10*</b></p>
-        <p><b>May 8</b></p>
-        <p><b>June 12</b></p>
-        <p><b>July 10*</b></p>
-        <p><b>August 14</b></p>
-        <p><b>September 11*</b></p>
-        <p><b>October 9</b></p>
-        <p><b>November 13</b></p>
-        <p><b>December 11</b></p>
+        <h3>2019 Meeting Schedule:</h3>
+        <p><b>January 11</b></p>
+        <p><b>February 8</b></p>
+        <p><b>March 8</b></p>
+        <p><b>April 12</b></p>
+        <p><b>May 10</b></p>
+        <p><b>June 14</b></p>
+        <p><b>July 12*</b></p>
+        <p><b>August 9*</b></p>
+        <p><b>September 13*</b></p>
+        <p><b>October 11</b></p>
+        <p><b>November 8</b></p>
+        <p><b>December 13</b></p>
 
         <!-- p class="finePrint">* This meeting will be held at the Sunrise Assisted Living Center at 1395 Massachusetts Ave, Arlington, MA 02476</p -->
-        <small class="finePrint">* This meeting will be held at the St Paul Lutheran Church in Arlington. Please see the <a href="directions.php">Meetings Location Page</a> for directions.</small>
+        <!-- <small class="finePrint">* This meeting will be held at the St Paul Lutheran Church in Arlington. Please see the <a href="directions.php">Meetings Location Page</a> for directions.</small> -->
+        <small class="finePrint">* This meeting will be held at an alternate location in Arlington.</small>
       </div>
     </section>
   </main>
