25a0600fd8efc945796c6dedbd99cf30d1750531
Jonathan Dallas <jonathan.dallas@velir.com>
2020-03-16 09:56:12 -0400

    Updated Header

--- include/header.php
+++ include/header.php
@@ -1,12 +1,12 @@
 <header class="header">
   <div class="header__container">
-    <h1 class="logo">
-      <a href="index.php">
-        <img src="image/ACPA_logo.gif" alt="American Chronic Pain Association" border="0" />
-        <span class="logo__subtext" aria-hidden>Arlington, MA Chapter</span>
-        <span class="logo__title visually-hidden">American Chronic Pain Association - Arlington, MA Chapter</span>
-      </a>
+    <h1 class="header__title">
+      <a href="index.php">Chronic Pain Support Group, Arlington, MA</a>
     </h1>
+    <a href="index.php" class="header__logo">
+      <img src="image/ACPA_logo.gif" alt="American Chronic Pain Association" border="0" />
+      <span class="header__logo-subtext" aria-hidden>Arlington, MA Chapter</span>
+    </a>
     <img class="logo2" src="image/us_pain_logo.jpg" alt="US Pain" />
   </div>
 </header>
