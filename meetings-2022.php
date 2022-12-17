fb2ef30b8763958d25cf1e27f9002b66601df562
Jonathan Dallas <jonathan.dallas@velir.com>
2022-12-17 09:25:22 -0500

    2023 Schedule

---
+++ meetings-2022.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Schedule of Meetings - Chronic Pain Support Group, Arlington, MA</title>
+  <?php include './include/head.php' ?>
+</head>
+<body>
+  <?php include './include/header.php' ?>
+  <main class="main">
+    <div class="main__nav">
+      <?php include './include/main-nav.php' ?>
+    </div>
+    <section class="main__content" id="main-content">
+      <div class="main__title"><h1>Schedule of Meetings</h1></div>
+      <div class="main__text rich-text">
+        <p>We meet the second Friday of every month from 11:00am to 12:30pm with an optional 12:30pm to 1:30pm social hour at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins (main) Library</a>, 700 Mass Ave. in the ground floor Community Room.  Please mark your calendar!</p>
+        <!-- p>We meet the second Friday of every month from 11:00am to 12:30pm with an optional 12:30pm to 1:30pm social hour at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins (main) Library</a>, 700 Mass Ave. in the ground floor Community Room.  Please note that meetings marked with an asterisk will be held at an alternate location.  Please mark your calendar!</p -->
+        <p class="alert-text">
+          During the COVID-19 pandemic, we will meet virtually using Zoom.  Please e-mail the leader for details.
+        </p>
+        <h2 class="h3">2022 Meeting Schedule:</h2>
+        <p><b>January 14</b></p>
+        <p><b>February 12</b></p>
+        <p><b>March 11</b></p>
+        <p><b>April 8</b></p>
+        <p><b>May 13</b></p>
+        <p><b>June 10</b></p>
+        <p><b>July 8</b></p>
+        <p><b>August 12</b></p>
+        <p><b>September 9</b></p>
+        <p><b>October 14</b></p>
+        <p><b>November 11</b></p>
+        <p><b>December 9</b></p>
+
+        <!-- p class="finePrint">* This meeting will be held at the Sunrise Assisted Living Center at 1395 Massachusetts Ave, Arlington, MA 02476</p -->
+        <!-- <small class="finePrint">* This meeting will be held at the St Paul Lutheran Church in Arlington. Please see the <a href="directions.php">Meetings Location Page</a> for directions.</small> -->
+        <!-- div>
+          <small class="finePrint">+ This meeting will begin at 10am and go until 11:30am.  This is Good Friday and the library is closing at noon.</small>
+        </div -->
+        <!-- div>
+          <small class="finePrint">* This meeting will be held at an alternate location in Arlington.</small>
+        </div -->
+      </div>
+    </section>
+  </main>
+  <?php include './include/foot.php' ?>
+</body>
+</html>
--- meetings.php
+++ meetings.php
@@ -13,33 +13,21 @@
     <section class="main__content" id="main-content">
       <div class="main__title"><h1>Schedule of Meetings</h1></div>
       <div class="main__text rich-text">
-        <p>We meet the second Friday of every month from 11:00am to 12:30pm with an optional 12:30pm to 1:30pm social hour at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins (main) Library</a>, 700 Mass Ave. in the ground floor Community Room.  Please mark your calendar!</p>
-        <!-- p>We meet the second Friday of every month from 11:00am to 12:30pm with an optional 12:30pm to 1:30pm social hour at the Arlington <a href="http://www.robbinslibrary.org" target="_blank">Robbins (main) Library</a>, 700 Mass Ave. in the ground floor Community Room.  Please note that meetings marked with an asterisk will be held at an alternate location.  Please mark your calendar!</p -->
-        <p class="alert-text">
-          During the COVID-19 pandemic, we will meet virtually using Zoom.  Please e-mail the leader for details.
-        </p>
-        <h2 class="h3">2022 Meeting Schedule:</h2>
-        <p><b>January 14</b></p>
-        <p><b>February 12</b></p>
-        <p><b>March 11</b></p>
-        <p><b>April 8</b></p>
-        <p><b>May 13</b></p>
-        <p><b>June 10</b></p>
-        <p><b>July 8</b></p>
-        <p><b>August 12</b></p>
-        <p><b>September 9</b></p>
-        <p><b>October 14</b></p>
-        <p><b>November 11</b></p>
-        <p><b>December 9</b></p>
-
-        <!-- p class="finePrint">* This meeting will be held at the Sunrise Assisted Living Center at 1395 Massachusetts Ave, Arlington, MA 02476</p -->
-        <!-- <small class="finePrint">* This meeting will be held at the St Paul Lutheran Church in Arlington. Please see the <a href="directions.php">Meetings Location Page</a> for directions.</small> -->
-        <!-- div>
-          <small class="finePrint">+ This meeting will begin at 10am and go until 11:30am.  This is Good Friday and the library is closing at noon.</small>
-        </div -->
-        <!-- div>
-          <small class="finePrint">* This meeting will be held at an alternate location in Arlington.</small>
-        </div -->
+        <p>We meet the second Friday of every month from 11:00am to 12:30pm.</p>
+        <p>We changed our meetings to virtual during the pandemic and for the foreseeable future.  Please email the leader at <a href="mailto:groupleader@chronicpainsupportboston.org">groupleader@chronicpainsupportboston.org</a> for details.  Please mark your calendar!</p>
+        <h2 class="h3">2023 Meeting Schedule:</h2>
+        <p><b>January 13</b></p>
+        <p><b>February 10</b></p>
+        <p><b>March 10</b></p>
+        <p><b>April 14</b></p>
+        <p><b>May 12</b></p>
+        <p><b>June 9</b></p>
+        <p><b>July 14</b></p>
+        <p><b>August 11</b></p>
+        <p><b>September 8</b></p>
+        <p><b>October 13</b></p>
+        <p><b>November 10</b></p>
+        <p><b>December 8</b></p>
       </div>
     </section>
   </main>
