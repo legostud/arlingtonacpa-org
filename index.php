d20e7ad147e089a5cefe1b65d3a8f6e8bd8f6580
Jonathan Dallas <jonathan.dallas@velir.com>
2024-10-25 15:59:00 -0400

    About Us page updates

--- index.php
+++ index.php
@@ -14,20 +14,14 @@
       <div class="main__title"><h1>About Us</h1></div>
       <div class="main__text rich-text">
         <!-- h2>April 14th Meeting - Time change: 10am - 11:30am Library closes at noon</h2 -->
-        <p>We are a support group for people living with chronic pain in the Boston, MA area and surrounding New England area.  Our group was founded in April, 2000 and has been meeting continuously year round since then. Once a month, we meet to discuss the challenges of living with chronic pain and offer support to each other. At our meetings, we can also hear updates on research and therapies from physicians and other professionals in the area. </p>
+        <p>We are a support group for people living with chronic pain from any condition, injury or diagnosis. Our group was founded in April, 2000 and has been meeting continuously all year around since then. Once a month, we meet to discuss the challenges of living with chronic pain and offer support to each other. At our meetings, we also invite guest speakers to present their updates on research and therapies.</p>
         <p>Our mission is to provide a safe place where people with chronic pain can support one another, develop friendships, share ideas and thoughts, obtain information about their condition from professionals and each other, and learn coping skills in a nonjudgmental and caring atmosphere.</p>
-        <p>Our meetings are free and open to anyone living with chronic pain. We meet at Robbins Library in Arlington, MA on the second Friday of the month, from 11:00am to 12:30pm. When that location is unavailable, we meet at an alternate location in Arlington.</p>
-        <p>We are affiliated with the US Pain Foundation, which is a national, non-profit organization that offers educational materials, support groups, and advocacy for people living with pain and their families and caregivers.  Our group and this national group helps individuals living with chronic pain find connection, hope and inspiration, and are dedicated to raising awareness about issues of living with chronic pain among the healthcare community, policy makers and the public-at-large.</p>
+        <p>Our meetings are free and open to anyone living with chronic pain. We meet virtually on the second Friday of the month, from 11:00am to 12:30pm et.</p>
+        <p>We are affiliated with the US Pain Foundation, which is a national, non-profit organization that offers educational materials, support groups, and advocacy for people living with pain. Our group helps individuals living with chronic pain find connection, hope and inspiration, and is dedicated to raising awareness about issues of living with chronic pain among the healthcare community, policy makers and the public-at-large.</p>
         <p>You may email us at: <a class="link link--mailto" href="mailto:groupleader@chronicpainsupportboston.org">groupleader@chronicpainsupportboston.org</a> or call <a class="link link--phone" href="+17816520146">781-652-0146.</a></p>
         <small>
           <h3>Disclaimer</h3>
-          <p>Our Chronic Pain Support Group is run by people with chronic pain who
-            volunteer their services. The group offers encouragement and information.
-            We are not medical professionals and do not make any representations,
-            warranties, or assurances as to the accuracy or completeness of the information
-            provided. We shall not be liable for any damages or injury resulting from
-            your access to this Internet site or from your reliance on any information
-            provided at this Internet site.
+          <p>Our Chronic Pain Support Group is run by people with chronic pain who volunteer their services. We are not medical professionals and do not make any representations, warranties, or assurances as to the accuracy or completeness of the information provided. We shall not be liable for any damages or injury resulting from your access to this Internet site or from your reliance on any information provided at this Internet site.
           </p>
         </small>
       </div>
