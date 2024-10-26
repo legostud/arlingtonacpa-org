cbc47e2c7d51d84697a7c927dda988a68841e4d6
Jonathan Dallas <jonathan.dallas@velir.com>
2024-10-26 13:57:52 -0400

    Cleaning up contact us page

--- contactus.php
+++ contactus.php
@@ -13,21 +13,8 @@
     <section class="main__content" id="main-content">
       <div class="main__title"><h1>Contact us</h1></div>
       <div class="main__text rich-text">
-        <p>For questions or to obtain the virtual link.</p>
+        <p>For questions or to obtain the virtual link</p>
         <p><em><strong>Group Leader</strong>: Cindy Steinberg, <a href="mailto:groupleader@chronicpainsupportboston.org">groupleader@chronicpainsupportboston.org</a></em></p>
-        <p><br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        Website designed by: <a href="http://www.leedscarroll.com/" target="_blank">Marion Leeds Carroll</a><br />
-        Website bulit and hosted by: <a href="http://www.jonathandallas.com" target="_blank">Jonathan Dallas</a></p>      
       </div>
     </section>
   </main>
